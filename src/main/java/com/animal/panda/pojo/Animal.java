f12bf3211ea5f4519d2bf1f713c3dd81fd01a0fd
xc <123@example.com>
2019-08-08 17:28:47 +0800

    添加异步调用功能

--- src/main/java/com/animal/panda/pojo/Animal.java
+++ src/main/java/com/animal/panda/pojo/Animal.java
@@ -20,6 +20,12 @@ public class Animal {
 
     private Integer type;
 
+    /**
+     *  @Transient表示该属性并非一个到数据库表的字段的映射,ORM框架将忽略该属性.
+     *  如果一个属性并非数据库表的字段映射,就务必将其标示为@Transient,否则,ORM框架默认其注解为@Basic
+     *  并抛出列不存在的sql异常
+     */
+    @Transient
     private String typeText;//动物类型文字
 
     /**
