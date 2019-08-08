ce788cbec25dd8f1b9dade0de159c08d0aed7a35
xc <123@example.com>
2019-08-08 16:25:08 +0800

    添加异步调用功能

--- src/main/java/com/animal/panda/PandaApplication.java
+++ src/main/java/com/animal/panda/PandaApplication.java
@@ -3,6 +3,7 @@ package com.animal.panda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -14,6 +15,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ComponentScan(basePackages= {"com.animal"})
 //开启定时任务
 @EnableScheduling
+//开启异步调用方法
+@EnableAsync
 public class PandaApplication {
 
     public static void main(String[] args) {
