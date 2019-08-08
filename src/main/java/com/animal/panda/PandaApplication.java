5875370f17850baac3c86f66bce8f4dcbb081fcc
xc <123@example.com>
2019-08-08 16:13:20 +0800

    添加定时任务功能

--- src/main/java/com/animal/panda/PandaApplication.java
+++ src/main/java/com/animal/panda/PandaApplication.java
@@ -3,6 +3,7 @@ package com.animal.panda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
@@ -11,6 +12,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.animal.panda.mapper")
 //扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
 @ComponentScan(basePackages= {"com.animal"})
+//开启定时任务
+@EnableScheduling
 public class PandaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/animal/panda/task/GetCurrentTimeTask.java
@@ -0,0 +1,19 @@
+package com.animal.panda.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class GetCurrentTimeTask {
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Scheduled(cron = "5 * * * * ? ")//每分钟的第五秒执行
+    public void getCurrentTime(){
+
+        System.out.println("当前时间:"+FORMAT.format(new Date()));
+
+    }
+}
